c33a37e90a2f689392680ae23cabeb6bbeea4160
Badre <badre-addine.fouad@devinci.fr>
2018-01-31 20:25:23 +0100

    cleaning repository

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-# On appelle la fonction setup
-setup(
-    name = "Mp_galafaleague",
-    version = "1",
-    description = "Projet application cloud",
-    executables = [Executable("CloudAppMongo.py")],
-)
